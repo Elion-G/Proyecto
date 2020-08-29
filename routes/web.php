b3eaba56f1dd4e56d8223c973dd42fce0054de29
Elion-G <eliassantiagonzales1909@gmail.com>
2020-08-29 12:00:02 -0400

    My first commit

---
+++ routes/web.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $users = App\User::get();
+    return view('welcome', ['users' => $users]);
+});
+
+
+Route::get('/profile/{id}', function ($id){
+
+    $user = App\User::find($id);
+
+    $posts = $user->posts()
+    ->with('category', 'image','tags')
+    ->withCount('comments')->get();
+    $videos = $user->videos()
+    ->with('category', 'image','tags')
+    ->withCount('comments')->get();
+
+    return view('profile', [
+        'user' => $user,
+        'posts' => $posts,
+        'videos' => $videos
+    ]);
+
+})->name('profile');
+
+
+Route::get('/level/{id}', function ($id){
+
+    $level = App\level::find($id);
+
+    $posts = $level->posts()
+    ->with('category', 'image','tags')
+    ->withCount('comments')->get();
+    $videos = $level->videos()
+    ->with('category', 'image','tags')
+    ->withCount('comments')->get();
+
+    return view('level', [
+        'level' => $level,
+        'posts' => $posts,
+        'videos' => $videos
+    ]);
+
+})->name('level');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
